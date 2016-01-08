6ba22dd2c0edaf4b9b894bb58a4a40ee0dad103e
bkzhn <absh.bktl@gmail.com>
2016-01-09 01:53:29 +0600

    seperate code

--- gof/main.cpp
+++ gof/main.cpp
@@ -2,27 +2,7 @@
 
 using namespace std;
 
-class Singleton {
-public:
-	static Singleton *getInstance();
-private:
-	Singleton() {}
-	static Singleton* instance;
-};
-
-Singleton* Singleton::instance = 0;
-Singleton* Singleton::getInstance()
-{
-	if (!instance) {
-		instance = new Singleton();
-		cout << "getInstance(): New instance\n";
-	}
-	else {
-		cout << "getInstance(): Existing instance\n";
-	}
-	
-	return instance;
-}
+#include "singleton.h"
 
 
 int main()
