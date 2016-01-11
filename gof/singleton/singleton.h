ebacead91c179d88bd504113742dd8b047de2f16
bkzhn <absh.bktl@gmail.com>
2016-01-11 23:01:08 +0600

    implementing method inside class #singleton

--- gof/singleton/singleton.h
+++ gof/singleton/singleton.h
@@ -1,21 +1,19 @@
 class Singleton {
 public:
-	static Singleton *getInstance();
+	static Singleton* getInstance() {
+		if (!instance) {
+			instance = new Singleton();
+			cout << "getInstance(): New instance\n";
+		}
+		else {
+			cout << "getInstance(): Existing instance\n";
+		}
+		
+		return instance;
+	}
 private:
 	Singleton() {}
 	static Singleton* instance;
 };
 
 Singleton* Singleton::instance = 0;
-Singleton* Singleton::getInstance()
-{
-	if (!instance) {
-		instance = new Singleton();
-		cout << "getInstance(): New instance\n";
-	}
-	else {
-		cout << "getInstance(): Existing instance\n";
-	}
-	
-	return instance;
-}
